8bf064163251e100edbe9fe0e114c46c79812f18
2929porno <90973515+2929porno@users.noreply.github.com>
2022-01-10 17:04:00 +0900

    サッカーデータ革命の内容を追記

---
+++ METcolor.R
@@ -0,0 +1,11 @@
+metcolor <- c("Degas", "Derain", "Egypt",
+              "Gauguin", "Greek", "Hiroshige", "Hokusai1", "Hokusai2",
+              "Hokusai3", "Homer1", "Homer2", "Ingres", "Isfahan1",
+              "Isfahan2", "Juarez", "Klimt", "Lakota", "Manet", "Monet", "Moreau",
+              "Morgenstern", "Nattier", "Navajo", "NewKingdom", "Nizami",
+              "OKeeffe1", "OKeeffe2", "Peru1", "Peru2", "Pillement", "Pissaro",
+              "Redon", "Renoir", "Robert", "Signac", "Stevens", "Tara", "Thomas",
+              "Tiepolo", "Troy", "Tsimshian", "VanGogh1", "VanGogh2", "VanGogh3",
+              "Veronese", "Wissing")
+
+met.brewer("Greek")
